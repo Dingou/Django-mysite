22cc17a974b56b9cebc32484bc08178c92c04e6f
Dingou <gundamfs@163.com>
2016-10-19 04:59:32 -0400

    update blog app

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b-0^_u!bhx=skf!ub*qhix)@206$0o$u%kx-pp5v18o77^p+7y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] 
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE_CLASSES = [
